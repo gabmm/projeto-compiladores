0d16e4337a38b84bc5b5b1ca7c6dccd81ac3abc0
Gabriel Martins da Costa Medeiros <67205923+gabmm@users.noreply.github.com>
2025-07-02 01:31:11 -0300

    add: interpretando novas funcoes

--- src/visitors/InterpretVisitor.java
+++ src/visitors/InterpretVisitor.java
@@ -50,6 +50,10 @@ public class InterpretVisitor extends Visitor {
         throw new RuntimeException("Erro: Variavel '" + varName + "' nao declarada para atribuicao.");
     }
 
+    private boolean isInteger(Object number) {
+        return number instanceof Integer;
+    }
+
     @Override
     public void visit(Prog node) {
         System.out.println("Visitando prog...");
@@ -116,78 +120,137 @@ public class InterpretVisitor extends Visitor {
     
     @Override
     public void visit(Assign node) {
-     
+        System.out.println("Visitando Assign...");
+        node.getRhs().accept(this);
+        node.getLhs().accept(this);
+        env.peek().put((String) operands.pop(), operands.pop()); // aqui é add var ou update var?
     }
     
     @Override
     public void visit(Return node) {
-
+        System.out.println("Visitando Return...");
+        for (Exp ret : node.getExps()) {
+            ret.accept(this);
+        }
+        returnMode = true;
     }
 
     @Override
     public void visit(If node) {
-
+        System.out.println("Visitando If...");
+        node.getCondition().accept(this);
+        if ((Boolean) operands.pop()) {
+            node.getThenCmd().accept(this);
+        } else if (node.getElseCmd() != null) {
+            node.getElseCmd().accept(this);
+        }
     }
 
     @Override
     public void visit(CallStmt node) {
+        System.out.println("Visitando CallStmt...");
+        Fun fun = funcs.get(node.getID()); // acho a funcao
+        if (fun != null) {
+            for (Exp arg : node.getArgs()) {
+                arg.accept(this); // empilho os argumentos
+            }
+            fun.accept(this); // rodo a funcao, que empilha os retornos
 
+            /*
+             * tenho os retornos no topo da pilha, preciso agora dar pop() na pilha e mapear
+             * o retorno as variaveis
+             */
+            for (LValue ret : node.getReturns()) {
+                ret.accept(this); // esse é o nome das variaveis
+                addVariable((String) operands.pop(), operands.pop());
+                // env.peek().put((String)operands.pop(), operands.pop());
+            }
+        }
     }
     
     @Override
     public void visit(Add node) {
-
+        System.out.println("Visitando Add...");
+        node.getLeft().accept(this);
+        node.getRight().accept(this);
+        Number left, right;
+        right = (Number) operands.pop();
+        left = (Number) operands.pop();
+        if (isInteger(right) && isInteger(left)) {
+            operands.push(Integer.valueOf(left.intValue() + right.intValue()));
+        } else {
+            operands.push(Float.valueOf(left.floatValue() + right.floatValue()));
+        }
     }
-
-  
 
     @Override
     public void visit(And node) {
-
+        System.out.println("Visitando And...");
+        node.getLeft().accept(this);
+        node.getRight().accept(this);
+        Boolean left, right;
+        right = (Boolean) operands.pop();
+        left = (Boolean) operands.pop();
+        operands.push(Boolean.valueOf(right && left));
     }
 
     @Override
     public void visit(Or node) {
-
+        System.out.println("Visitando Or... (nem existe...)");
     }
 
     @Override
     public void visit(Eq node) {
-
+        System.out.println("Visitando EQ...");
+        node.getLeft().accept(this);
+        node.getRight().accept(this);
+        operands.push(Boolean.valueOf(operands.pop().equals(operands.pop())));
     }
 
     @Override
     public void visit(Neq node) {
-
+        System.out.println("Visitando NEQ...");
+        node.getRight().accept(this);
+        node.getLeft().accept(this);
+        operands.push(!Boolean.valueOf(operands.pop().equals(operands.pop())));
     }
     
     @Override
     public void visit(Var node) {
-        
+        System.out.println("Visitando VAR...");
+        // Object value = env.peek().get(node.getName());
+        Object value = findVariable(node.getName());
+        operands.push(value);
     }
     
     @Override
     public void visit(NInt node) {
+        System.out.println("Visitando NINT...");
         operands.push(Integer.valueOf(node.getValue()));
     }
 
     @Override
     public void visit(NFloat node) {
+        System.out.println("Visitando NFLOAT...");
         operands.push(Float.valueOf(node.getValue()));
     }
 
     @Override
     public void visit(NBool node) {
+        System.out.println("Visitando NBOOL...");
         operands.push(Boolean.valueOf(node.getValue()));
     }
 
     @Override
     public void visit(NChar node) {
+        System.out.println("Visitando NCHAR...");
         operands.push(Character.valueOf(node.getValue()));
     }
 
     @Override
-    public void visit(Null node) {  }
+    public void visit(Null node) {
+
+    }
 
     @Override
     public void visit(New node) {
@@ -208,6 +271,7 @@ public class InterpretVisitor extends Visitor {
     public void visit(Data node) {
         
     }
+
     @Override public void visit(TyInt node) {}
     @Override public void visit(TyChar node) {}
     @Override public void visit(TyBool node) {}
@@ -235,22 +299,58 @@ public class InterpretVisitor extends Visitor {
 
     @Override
     public void visit(Sub node) {
-
+        System.out.println("Visitando Sub...");
+        node.getLeft().accept(this);
+        node.getRight().accept(this);
+        Number left, right;
+        right = (Number) operands.pop();
+        left = (Number) operands.pop();
+        if (isInteger(right) && isInteger(left)) {
+            operands.push(Integer.valueOf(left.intValue() - right.intValue()));
+        } else {
+            operands.push(Float.valueOf(left.floatValue() - right.floatValue()));
+        }
     }
 
     @Override
     public void visit(Mul node) {
-
+        System.out.println("Visitando Mul...");
+        node.getLeft().accept(this);
+        node.getRight().accept(this);
+        Number left, right;
+        right = (Number) operands.pop();
+        left = (Number) operands.pop();
+        if (isInteger(right) && isInteger(left)) {
+            operands.push(Integer.valueOf(left.intValue() * right.intValue()));
+        } else {
+            operands.push(Float.valueOf(left.floatValue() * right.floatValue()));
+        }
     }
 
     @Override
     public void visit(Div node) {
-
+        System.out.println("Visitando Div...");
+        node.getLeft().accept(this);
+        node.getRight().accept(this);
+        Number left, right;
+        right = (Number) operands.pop();
+        left = (Number) operands.pop();
+        if (isInteger(right) && isInteger(left)) {
+            operands.push(Integer.valueOf(left.intValue() / right.intValue()));
+        } else {
+            operands.push(Float.valueOf(left.floatValue() / right.floatValue()));
+        }
     }
 
     @Override
     public void visit(Mod node) {
-
+        System.out.println("Visitando Add...");
+        node.getLeft().accept(this);
+        node.getRight().accept(this);
+        int left, right;
+        right = (Integer) operands.pop();
+        left = (Integer) operands.pop();
+        operands.push(Integer.valueOf(left % right));
     }
 
     @Override
@@ -260,12 +360,19 @@ public class InterpretVisitor extends Visitor {
 
     @Override
     public void visit(NNot node) {
-
+        node.getExp().accept(this);
+        operands.push(!(Boolean) operands.pop());
     }
 
     @Override
     public void visit(Neg node) {
-
+        node.getExp().accept(this);
+        Number exp = (Number) operands.pop();
+        if (isInteger(exp)) {
+            operands.push(Integer.valueOf(exp.intValue()));
+        } else {
+            operands.push(Float.valueOf(exp.floatValue()));
+        }
     }
 
 
